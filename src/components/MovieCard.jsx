abe8a0879c34d662b0f9fa414f34d91407c8926c
João Victor <joaovictor.developer.frontend@gmail.com>
2023-06-02 16:30:03 -0300

    animação de entrada na busca alterado

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -5,8 +5,8 @@ import './MovieCard.css'
 
 function MovieCard({id, img, title, lancamento}) {
   return (
-  <Link key={id} to={`/movie/${id}`}>
-      <div data-aos="fade-up" className="card">
+  <Link data-aos="zoom-in-up" key={id} to={`/movie/${id}`}>
+      <div className="card">
         <img src={img} alt="Poster" />
         <h3>{title}</h3>
         <p>{lancamento.substr(0, 4)}</p>
